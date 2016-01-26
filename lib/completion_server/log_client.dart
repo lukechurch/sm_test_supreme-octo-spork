94220cf41d00714febe13ca7ee9f192324109638
Luke Church <lukechurch@google.com>
2016-01-26 18:20:13 +0000

    Trim and flush logs

--- lib/completion_server/log_client.dart
+++ lib/completion_server/log_client.dart
@@ -14,7 +14,10 @@ enum LogTarget {
 }
 
 main(List<String> args) async {
-  for (int i = 0; i < 100; i++)  await info("test", "Test line");
+  for (int i = 0; i < 100000; i++)  {
+    await info("test", "=====================================");
+    await new Future.delayed(new Duration(milliseconds: 5000));
+  }
 }
 
 const LogTarget TARGET = LogTarget.LOG_SERVER;
@@ -23,21 +26,19 @@ const int LOG_SERVER_PORT = 9991;
 Future info(String srcName, String logItem) async {
   int i = 0;
 
-  print (i++);
   String logLine = "${new DateTime.now().toIso8601String()}: $srcName: $logItem";
-  print (i++);
+  logLine = logLine.substring(0, logLine.length > 100 ? 100 : logLine.length);
   switch (TARGET) {
     case LogTarget.STDOUT:
       print (logLine);
       break;
     case LogTarget.LOG_SERVER:
-    print (i++);
       return new HttpClient().post(
         InternetAddress.LOOPBACK_IP_V4.host, LOG_SERVER_PORT, '/').then((req) {
-          print (i++);
           req.write(logLine);
-          print (i++);
-          return req.close();
+          return req.flush().then((_) {
+            return req.close();
+          });
         });
   }
 }
