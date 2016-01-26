6ba9526ff78683452b1f6a9aecff9552ee9eecb4
Luke Church <lukechurch@google.com>
2016-01-26 13:01:01 +0000

    Test reimplement log_client

--- lib/completion_server/log_client.dart
+++ lib/completion_server/log_client.dart
@@ -5,26 +5,39 @@
 library smart.completion_server.log_client;
 
 import 'dart:io';
+import 'dart:async';
+
 
 enum LogTarget {
   LOG_SERVER,
   STDOUT
 }
 
+main(List<String> args) async {
+  for (int i = 0; i < 100; i++)  await info("test", "Test line");
+}
+
 const LogTarget TARGET = LogTarget.LOG_SERVER;
 const int LOG_SERVER_PORT = 9991;
 
-info(String srcName, String logItem) async {
+Future info(String srcName, String logItem) async {
+  int i = 0;
+
+  print (i++);
   String logLine = "${new DateTime.now().toIso8601String()}: $srcName: $logItem";
+  print (i++);
   switch (TARGET) {
     case LogTarget.STDOUT:
       print (logLine);
       break;
     case LogTarget.LOG_SERVER:
-      var request = await new HttpClient().post(
-        InternetAddress.LOOPBACK_IP_V4.host, LOG_SERVER_PORT, '/');
-      request.write(logLine);
-      await request.close();
-      break;
+    print (i++);
+      return new HttpClient().post(
+        InternetAddress.LOOPBACK_IP_V4.host, LOG_SERVER_PORT, '/').then((req) {
+          print (i++);
+          req.write(logLine);
+          print (i++);
+          return req.close();
+        });
   }
 }
